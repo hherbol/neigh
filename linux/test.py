9e10c7ebddbe610933a5d9ba67bde7d46979f1db
Henry Herbol <hherbol@gmail.com>
2016-04-23 22:20:56 -0400

    Updates

--- linux/test.py
+++ linux/test.py
@@ -1,26 +1,21 @@
 from neigh import gen_neigh
 from random import random # Rand 0.0 <= val < 1.0
+import time
 
-LENGTH = 10.0
-N = 10
-CUTOFF = 5.2
-
-
+X, Y, Z = 1000,1000,1000
+o1, o2, o3 = 0, 0, 0
+N = 8000
+CUTOFF = 10.0
 
 positions = [None for i in range(N)]
 for i in range(N):
-	x, y, z = random()*LENGTH, random()*LENGTH, random()*LENGTH
+	x, y, z = random()*X+o1, random()*Y+o2, random()*Z+o3
 	positions[i] = [x,y,z]
 
-print("\nPOSITIONS: %s" % str(positions))
+print("\nStarting..."),
+t0 = time.clock()
+neighbours = gen_neigh(positions, CUTOFF, PBC=[X,Y,Z], origin=[o1,o2,o3])
+print(" Done\n")
 
-print("\n\nA..."),
-neighbours = gen_neigh(positions, CUTOFF)
-print("B...\n")
-
-print("\n\nNeighs: %s" % str(neighbours))
-
-#print("Returned %d" % neighbours)
-
-#neighbours2 = gen_neigh(positions, R = 1, PBC=[100.0, 100.0, 100.0])
+print("Time taken = %.6f" % (time.clock()-t0))
 
